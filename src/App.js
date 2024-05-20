e8307eba5fe790618a0aedeed32558d8b1184457
BrandonHoffey <brandon.hoffey@yahoo.com>
2024-05-20 12:53:28 -0600

    Header started MainPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/mainPage/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Changed
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Header />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/components/mainPage/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const EllieMainPage = () => {
+  return (
+    <div>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Beth+Ellen&family=Cedarville+Cursive&family=Homemade+Apple&family=Josefin+Slab:wght@200&family=Julius+Sans+One&family=Oooh+Baby&family=Oswald:wght@200;400&display=swap"
+          rel="stylesheet"
+        />
+        <script src="../dist/index.js" defer></script>
+        <title>Ellie Hoffey</title>
+      </head>
+      <body>
+        <div id="header">
+          <h1>Ellie Hoffey</h1>
+        </div>
+      </body>
+    </div>
+  );
+};
+
+export default EllieMainPage;
